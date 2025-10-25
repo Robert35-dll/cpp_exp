fd84fd74da5f98821260ab50cae385a876aed99b
Robert Koch <Robert.Koch@Student.HTW-Berlin.de>
2025-10-25 19:08:21 +0200

    Arrays and containers (#1)
    * Added notes about arrays and containers
    
    * Added dummy solution to A3.1
    Ensured only positive integers are processed
    Printed all saved values after breaking input manually
    Implemented deletion of elements equal 5 after reaching limit of inputs' amount
    TODO: Add array's resize function
    
    * Extracted values removal
    The function is called EraseValues()
    
    * Implemented copying values to an array
    Also added overload of PrintContainer() to output c-arrays
    
    * Minor loop adjustments
    Extracted the MAX_SIZE constant used in the loop and the check statement after.
    Also swapped return statement to allow copying even with not full vectors.
    
    * Added doc strings
    
    * Added solution to A3.2
    Covered most use cases except target removal
    
    * Improved process commenting in RunBookStore()
    Added step marks into the loop
    
    * Added targeted removal of books
    
    * Added zero size checks for printing containers
    
    * Refactored input in A3.1
    Instead try-catch block a separate function is used.
    Also fixed a bug when invalid inputs were accepted anyway
    
    * Added doc strings to A3.2
    
    * Fixed a bug with permanent book removal
    
    * Minor validation fix
    Changed the checking of the first letter to exclude silly inputs :)
    Note that this doesn't help against multiple words

---
+++ A3/Arrays_and_Containers.md
@@ -0,0 +1,239 @@
+# Arrays and Containers
+
+Here are some basic notes about usage of arrays, containers and structs in C++.
+
+## C-native Structures
+
+These structures are quite low-level options of common ones used in other languages.
+Use them, if you're sure what you're doing. Otherwise go to [C-Structures](#c-structures).
+
+### C-Arrays
+
+Just like an array in any other compiled language - static, not resizable.
+
+```c
+int test = 1;
+
+// "Reserving" 10 values (all set to 0)
+int numbers1[10];
+// Assigning values directly
+int numbers2[2] = { 23 };
+// Assigning values alternatively by a variable
+int numbers3[3] = { test };
+
+// The size of an array is set automatically
+int numbers4[] = { 1, 2, 3 };
+
+// There're ofc multi-dimensional arrays as well
+int table[10][10] = { 2 };
+```
+
+However there's a tricky (_actually dangerous_) feature hiding behind the lines. 
+Most (OOP) languages (_including C++_) strongly restrict accessing arrays elements by its bounds (_0 to arrays' length - 1_). If you try to overcome these bounds, you get some kind of `IndexOutOfBoundsException`. C is kind of not designed to check these by default:
+
+```c
+// Creating an array of length 3
+int numbers[3] = { 1 };
+// so the last element is stored at index 2
+numbers[2] = 2;
+
+// however...
+
+// This is a valid operation :3
+int numbers[3] = 3;
+for (int i = 0; i < 4; i++) {
+    // The values should differ by 1
+    cout << "cArray[" << i << "]: " << numbers1[i] << endl;
+    // and their addresses by 4 (standard int's size)
+    cout << "cArray Address[" << i << "]: " << &numbers1[i] << endl;
+}
+```
+
+In the above code a value has been written to a memory space not associated with the `numbers` array at all.
+Such failures can lead to so called _segmentation faults_ (_accessing memory not reserved by a whole program_) and unexpected behavior, which is the most difficult to debug.
+
+### C-Strings
+
+Strings in C are represented as `char` arrays with trailing `\0` element.
+
+```c
+// Normal char array
+char myString[6] = { 'b','a','k','u','d','a','n' };
+// CString
+char myCString[7] = { 'b','a','k','u','d','a','n','\0' };
+```
+
+The `<cstring>` header file provides lots of functions to work with these.
+There're also many of extensions of _cstrings_ made by different companies and frameworks.
+
+### Unions
+
+Unions are specific C structs that combine multiple values into a single type.
+
+```c
+union newUnion {
+    // Only one field can be assigned automatically
+    unsigned int __int16 ui16 = 0;
+    unsigned int __int32 ui32;
+}
+```
+
+There're some key features unions have:
+- All fields are stored in **the same** memory segment
+- The size of the segment is set to fit the biggest field of a union
+
+Since by writing into a smaller field the bigger one will be changed as well, unions are most likely to be avoided nowadays.
+
+## C++ Structures
+
+These are practically extending the usage and increase safety of previous [C-native structures](#c-native-structures) by providing decent amount of additional manipulation methods.
+
+### STL Arrays
+
+Just like [C-Array](#c-arrays) but with `IndexOutOfBoundsException`:
+
+```c++
+#include <array>
+
+// Creating a C++ array
+std::array<int, 10> stlArray;
+// Iterating through it (specific size_t type)
+for (size_t i = 0; i < stlArray.size(); i++) {
+    stlArray[i] = i;
+    // Alternatively (also safer)
+    stlArray.at(i) = i;
+}
+
+// Alternatively getting the size of an array
+const size_t maxsize = stlArray.max_size();
+```
+
+Funny notice:
+```c++
+// This works
+int nums[2] = { 1 };
+cout << nums[2] << endl;
+
+// This likely not (may depend on compiler)
+string strs[2] = { "1" };
+cout << nums[2] << endl;    // Error: Segmentation Fault
+```
+
+### Custom Data Types
+
+In C++ there's a feature that allows you to rename certain data types or define a new one:
+
+```c++
+// Renaming an existing data type
+typedef unsigned int uint;
+// Creating a "new" one
+typedef int[100] int_array;
+
+// Purely renaming a data type (will give cursed errors, not preferred)
+#define MY_UINT unsigned int
+```
+
+### Structs
+
+Just like [unions](#unions) in C but w/o _memory sparing complex_ xD
+
+```c++
+// Defining a struct as high as possible
+// to let compiler know as early as possible
+struct shoe {
+    unsigned short usSize = 0;
+    string brand = "notSpecified";
+    eZipper zipper = notSet;
+    string colorName = "notSpecified";
+};
+
+shoe shoe1;
+shoe shoe2;
+
+// Comparing bare structs is performed bitwise
+cout << shoe1 < shoe2 << endl;
+
+// Overwriting comparison operator (also overloading)
+bool operator < (const shoe &a, const shoe &b) {
+    return a.UsSize < b.UsSize;
+}
+```
+
+Structs are typically used to combine values into a single variable without defining any methods to modify them (_otherwise it would be a class_).
+
+### Enums
+
+Enums are practically a stricter form of structs (_also available in C_):
+
+```c++
+// Enums cannot be changed after declaration
+enum eZipper {
+    // They also can contain only data types
+    Simple = 3,
+    Modern = 1,
+    Western = 0
+}
+```
+
+Enums are the perfect choice for cases, when a static option has to be used.
+
+### STL Vectors
+
+Vectors provide a decent flexibility of managing and iterating over a series of values.
+Other languages name vectors something like _Collections_ or _Lists_.
+There're lots of methods a vector has to offer:
+
+```c++
+#include <vector>
+
+// Defining a simple vector
+std::vector<double> drunk;
+
+// Adding a new element to the end
+drunk.push_back(1.0);
+// Adding a new element at certain index
+drunk.insert(drunk.begin() + 1, 2.0);
+
+// Removing an element at the end
+drunk.pop_back();
+// Remove an element at index 1
+drunk.erase(drunk.begin() + 1);
+// Remove all elements
+drunk.clear();
+
+// Access elements
+drunk[0];
+drunk.front();
+drunk.back();
+
+// Access element at the beginning of a vector
+drunk.begin();
+// Access element behind the end of a vector
+drunk.end();
+
+// Getting the vector's size (for iterations)
+drunk.size();
+
+// Reserving memory for another 1000 elements
+drunk.reserve(1000);
+// Shrinking the vector (possible data loss after 500th element)
+drunk.resize(500);
+// Shrinking the vector (no data loss)
+drunk.shrink_to_fit();
+
+// Iterating through a vector (just like an array)
+for (size_t i = 0; i < drunk.size(); i++) {}
+
+// or with an iterator
+vector<float>::iterator i;
+for (i = drunk.begin(); i != drunk.end(); i++) {}
+
+// Alternative iterator
+for (auto &a : drunk) {}
+
+// Additionally swapping the whole vector
+std::vector<double> eaten;
+drunk.swap(eaten);
+```
+
+Keep in mind, that an iterator represents an address of a vector's element (_a pointer_).
---
+++ A3/code_a3.cpp
@@ -0,0 +1,545 @@
+#include <iostream>
+#include <array>
+#include <vector>
+
+#include <cmath>
+
+using namespace std;
+
+// A3.1 Functions
+void ReadSeries();
+vector<int> EraseValues(vector<int>, int);
+void CopyToArray(vector<int>, int[]);
+
+double GetDoubleInput(string requestLine = "Geben Sie eine Gleitkommazahl ein",
+                      bool isSigned = true);
+void PrintContainer(vector<int>);
+void PrintContainer(int[], size_t);
+
+// A3.2 Functions and structs
+struct Book {
+    int id = 0;
+    string name = "NotSpecified";
+    int year = 1970;
+};
+
+void RunBookStore();
+int GetManagementOption();
+Book CreateNewBook();
+
+int FindBookIndex(vector<Book>, string);
+int FindBookIndex(vector<Book>, int);
+
+int GetIntInput(string requestLine = "Geben Sie eine ganze Zahl ein",
+                bool isSigned = true);
+string GetStringInput(string requestLine = "Geben Sie einen Titel ein");
+
+void PrintContainer(vector<Book>);
+
+int main() {
+    // A3.1
+    cout << "[*] A3.1" << endl;
+    ReadSeries();
+    cout << " |" << endl;
+
+    // A3.2
+    cout << "[*] A3.2" << endl;
+    RunBookStore();
+
+    return 1;
+}
+
+#pragma region [A3.1]
+
+/**
+ * @brief Reads up to 50 positive ints into a vector and copies these
+ *        into an array afterwards.
+ * @note If max capacity is reached, all values equal 5 will be removed :)
+ */
+void ReadSeries() {
+    // Defining required input variables and structures
+    const size_t MAX_SIZE = 50;
+
+    vector<int> inputSeries;
+    double inputNumber;
+    
+    // Reading maximum 50 positive ints
+    while (inputSeries.size() < MAX_SIZE) {
+        // Requesting an input
+        inputNumber = GetDoubleInput(
+            "Geben Sie eine ganze positive Zahl oder -1 ein"
+        );
+        
+        // Skipping the iteration upon negative values less than -1
+        if (inputNumber < -1) {
+            cout << "[!] Es muss doch eine positive Zahl sein >:(\n |" << endl;
+            continue;
+        }
+        // or upon non-integer numbers
+        //* Thanks to Zeilinger for the idea of this type check
+        //* https://cplusplus.com/forum/general/41273/#msg222521
+        if (inputNumber != floor(inputNumber)) {
+            cout << "[!] Es muss doch eine ganze Zahl sein >:(\n |" << endl;
+            continue;
+        }
+
+        // Printing converted value (for debug's sake)
+        cout << " |-< Eingelesene Zahl: " << inputNumber << endl;
+        
+        // Breaking inputs upon receiving -1 and logging saved values
+        if (inputNumber == -1) {
+            cout << " |-<!> Eingabe unterbrochen" << endl;
+            cout << "[<] Gespeicherte Werte:" << endl;
+            
+            PrintContainer(inputSeries);
+            
+            break;
+        }
+        
+        // Saving the input
+        //* Note: floats are converted to ints automatically
+        inputSeries.push_back(inputNumber);
+    }
+
+    // If nothing was put -> do nothing :)
+    if (inputSeries.size() == 0) {
+        cout << " |-<!> Keine Werte wurden gespeichert." << endl;
+        cout << "[^] Beende das Programm." << endl;
+        return;
+    }
+    
+    // Removing all elements that equal maximum size, if vector is full
+    if (inputSeries.size() == MAX_SIZE) {
+        cout << " |-<*> Entferne Werte gleich 5" << endl;
+        // This might become more efficient with pointers yet to be learned
+        inputSeries = EraseValues(inputSeries, 5);
+    }
+
+    // Defining the output array and copying values into it
+    int outputSeries[inputSeries.size()];
+
+    cout << " |-<*> Kopiere Werte aus dem Vektor in einen Array" << endl;
+    CopyToArray(inputSeries, outputSeries);
+
+    // Finally printing the output array
+    cout << "[<] Kopierte Werte:" << endl;
+    PrintContainer(outputSeries, inputSeries.size());
+}
+
+/**
+ * @brief Removes certain value from a vector.
+ * @note This function might work more efficient
+ *       using pointers yet to be learned.
+ * @param v: The vector to remove values from.
+ * @param target: The value to remove.
+ * @retval A vector without specified values.
+ */
+vector<int> EraseValues(vector<int> v, int target) {
+    // Iterating through the vector
+    for (size_t i = 0; i < v.size(); i++) {
+        // Skipping non-target values
+        if (v.at(i) != target) {
+            continue;
+        }
+        
+        // and removing the right ones
+        //* Note: removing a value shrinks the vector by 1
+        v.erase(v.begin() + i);
+        i--;
+    }
+
+    return v;
+}
+
+/**
+ * @brief Writes values from a vector into an array.
+ * @note The array is to be assumed to be of the same length as the vector.
+ * @param v: The vector to copy values from.
+ * @param a[]: The array to copy values into.
+ */
+void CopyToArray(vector<int> v, int a[]) {
+    for (size_t i = 0; i < v.size(); i++) {
+        a[i] = v.at(i);
+    }
+}
+
+/**
+ * @brief Retrieves a `double` number from the user.
+ * @note This function keeps asking for input until a convertible string
+ *       is provided.
+ * @param requestLine: The line to request an input with.
+ * @param isSigned: Whether the number has to be positive or not.
+ * @retval A `double` number provided by user.
+ */
+double GetDoubleInput(string requestLine, bool isSigned) {
+    // Defining required variables
+    string rawInput;
+    double numberInput;
+    
+    while (true) {
+        // Asking for input
+        cout << "[>] " << requestLine << ": ";
+        cin >> rawInput;
+
+        // Trying to convert the input to float,
+        // if succeeded -> break the loop,
+        // if failed -> skip the iteration
+        try {
+            numberInput = stof(rawInput);
+            if (!isSigned && numberInput < 0) {
+                throw range_error("Keine positive Zahl eingegeben.");
+            }
+        } catch (invalid_argument) {
+            cout << "[!] Es muss doch eine Zahl sein!\n |" << endl;
+            continue;
+        } catch (range_error) {
+            cout << "[!] Es muss doch eine positive Zahl sein!\n |" << endl;
+            continue;
+        }
+        
+        break;
+    }
+
+    return numberInput;
+}
+
+/**
+ * @brief Outputs a container into CLI.
+ * @note If the vector is empty, a respective message
+ *       will be printed instead.
+ * @param v: The vector to output values of.
+ */
+void PrintContainer(vector<int> v) {
+    if (v.size() == 0) {
+        cout << " |-<!> Es gibt nichts :(" << endl;
+        return;
+    }
+
+    for (size_t i = 0; i < v.size(); i++) {
+        cout << " |-< [" << i << "]: " << v.at(i) << endl;
+    }
+}
+
+/**
+ * @brief Outputs a container into CLI.
+ * @note If the array is empty, a respective message
+ *       will be printed instead.
+ * @param a[]: The array to output values of.
+ * @param size: The size of the array.
+ */
+void PrintContainer(int a[], size_t size) {
+    if (size == 0) {
+        cout << " |-<!> Es gibt nichts :(" << endl;
+        return;
+    }
+
+    for (size_t i = 0; i < size; i++) {
+        cout << " |-< [" << i << "]: " << a[i] << endl;
+    }
+}
+
+#pragma endregion [A3.1]
+#pragma region [A3.2]
+
+/**
+ * @brief Starts a program that operates a simple book store.
+ * @note There're several options to add, remove and list saved books.
+ */
+void RunBookStore() {
+    //* Operating process:
+    //*
+    //* Step 1: Ask for input of a new book     -> Step 2
+    //*         or for output of stored books   -> Step 3
+    //*         or for exit                     -> Step 1*
+    //*         |
+    //* Step 1*:Prevent future requests and exit
+
+    //* Step 2: Create new struct object via cin
+    //*         add to the store (vector)
+    //*         validate input
+    //*         |
+    //* Step 2*:If input's wrong -> remove and go to step 2
+    //*         If input's right -> go to step 1
+
+    //* Step 3: List all available books
+    //*         Ask for removal of a book (per index or name)
+    //*         |
+    //* Step 3*:If requested -> remove the book and go to step 1
+    //*         If not       -> just go to step 1
+
+    // Defining required input variables and structures
+    vector<Book> bookStore;
+
+    string validationOption;
+    int option = 0;
+
+    // Reading inputs and operating the store
+    while (option != -1) {
+        cout << " |" << endl;
+
+        // Step 1: Asking for input or output
+        option = option == 0 ? GetManagementOption() : option;
+
+        switch (option) {
+            // Step 1.1: Exiting program
+            case -1: {
+                cout << " |-<*> Beende das Programm" << endl;
+                continue;
+            }
+            // Step 2: Adding a new book and validating it
+            case 1: {
+                cout << " |-<*> Erstelle neues Buch..." << endl;
+                Book newBook = CreateNewBook();
+                bookStore.push_back(newBook);
+
+                validationOption = GetStringInput(
+                    "Ist das neue Buch falsch eingetragen? [j / n]"
+                );
+                break;
+            }
+            // Step 3: Listing available books and validating them
+            case 2: {
+                PrintContainer(bookStore);
+
+                if (bookStore.size() != 0) {
+                    validationOption = GetStringInput(
+                        "Wollen Sie ein Buch entfernen? [j / n]"
+                    );
+                } else {
+                    validationOption = "n";
+                }
+                break;
+            }
+            default: {
+                cout << "[!] Ungültige Option. ";
+                cout << "Bitte prüfen Sie ihre Eingabe.\n |" << endl;
+
+                option = 0;
+                continue;
+            }
+        }
+
+        // If validation failed -> do respective substeps
+        if (validationOption.length() >= 1 && validationOption.at(0) == 'j') {
+            // Step 2*
+            // If an input's incorrect -> remove last book and go to Step 2
+            if (option == 1) {
+                cout << " |-<*> Entferne das Buch..." << endl;
+                bookStore.pop_back();
+
+                continue;
+            }
+            // Step 3*
+            // If removal requested -> remove the book and go to Step 1
+            if (option == 2) {
+                string bookId = GetStringInput(
+                    "Geben Sie den Titel oder die (ID) vom Buch ein"
+                );
+
+                // Looking for the book and removing it
+                int bookIndex = FindBookIndex(bookStore, bookId);
+                if (bookIndex != -1) {
+                    cout << " |-<*> Entferne das Buch..." << endl;
+                    bookStore.erase(bookStore.begin() + bookIndex);
+                }
+
+                option = 0;
+                continue;
+            }
+        }
+
+        // Returning to Step 1
+        option = 0;
+    }
+
+    cout << "[^] Programm beendet" << endl;
+}
+
+/**
+ * @brief Asks for a choice of certain pre-defined options.
+ * @retval The number of chosen option as `int`.
+ */
+int GetManagementOption() {
+    // Defining required input variable
+    int option;
+
+    while (true) {
+        // Printing options
+        cout << "[?] Was wollen Sie tun?\n |" << endl;
+        cout << "[1] Neues Buch hinzufügen" << endl;
+        cout << "[2] Vorhandene Bücher auflisten" << endl;
+        cout << " |" << endl;
+        cout << "[-1] Programm beenden" << endl;
+        cout << " |" << endl;
+
+        // Reading input,
+        // if option's valid -> break the loop and return it,
+        // if option's invalid -> iterate again
+        option = GetIntInput();
+        if (option == -1 || option == 1 || option == 2) {
+            break;
+        }
+
+        cout << "[!] Bitte geben Sie eine gültige Option ein!\n |" << endl;
+    }
+
+    return option;
+}
+
+/**
+ * @brief Asks for several inputs to create a new `Book` object.
+ * @retval A new `Book` object.
+ */
+Book CreateNewBook() {
+    Book newBook;
+    newBook.id = GetIntInput("Geben Sie die ID vom Buch ein", false);
+    newBook.name = GetStringInput("Geben Sie den Titel vom Buch ein");
+    newBook.year = GetIntInput("Geben Sie das Erscheinungsjahr ein");
+
+    return newBook;
+}
+
+/**
+ * @brief Searches for a book with specified title in the given vector.
+ * @note If title can be converted to an int, a respective overloaded
+ *       function will be called instead.
+ * @param v: The vector to search the book in.
+ * @param target_name: The title of the book to seach.
+ * @retval The index of the found book withing the given vector or
+ *         -1, if nothing was found.
+ */
+int FindBookIndex(vector<Book> v, string target_name) {
+    // Checking, whether the name is an ID of a book,
+    // if not -> search by the name instead
+    try {
+        int target_id = stoi(target_name);
+        return FindBookIndex(v, target_id);
+    } catch (invalid_argument) {}
+
+    cout << " |-<*> Suche das Buch nach dem Titel..." << endl;
+
+    for (size_t i = 0; i < v.size(); i++) {
+        if (v.at(i).name == target_name) {
+            return i;
+        }
+    }
+
+    cout << " |-<!> Kein Buch mit dem Titel '";
+    cout << target_name << "' gefunden :(" << endl;
+    return -1;
+}
+
+/**
+ * @brief Searches for a book with specified ID in the given vector.
+ * @param v: The vector to search the book in.
+ * @param target_id: The ID of the book to seach.
+ * @retval The index of the found book withing the given vector or
+ *         -1, if nothing was found.
+ */
+int FindBookIndex(vector<Book> v, int target_id) {
+    cout << " |-<*> Suche das Buch nach der ID..." << endl;
+
+    for (size_t i = 0; i < v.size(); i++) {
+        if (v.at(i).id == target_id) {
+            return i;
+        }
+    }
+
+    cout << " |-<!> Kein Buch mit der ID '";
+    cout << target_id << "' gefunden :(" << endl;
+    return -1;
+}
+
+/**
+ * @brief Retrieves an `int` number from the user.
+ * @note This function keeps asking for input until a convertible string
+ *       is provided.
+ * @param requestLine: The line to request an input with.
+ * @param isSigned: Whether the number has to be positive or not.
+ * @retval An `int` number provided by user.
+ */
+int GetIntInput(string requestLine, bool isSigned) {
+    // Defining required input variables
+    string rawInput;
+    int numberInput;
+
+    while (true) {
+        // Asking for input
+        cout << "[>] " << requestLine << ": ";
+        cin >> rawInput;
+
+        // Trying to convert the input to int,
+        // if succeeded -> break the loop,
+        // if failed -> skip the iteration
+        try {
+            numberInput = stoi(rawInput);
+            if (!isSigned && numberInput < 0) {
+                throw range_error("Keine positive Zahl eingegeben.");
+            }
+        } catch (invalid_argument) {
+            cout << "[!] Es muss doch eine Zahl sein!\n |" << endl;
+            continue;
+        } catch (range_error) {
+            cout << "[!] Es muss doch eine positive Zahl sein!\n |" << endl;
+            continue;
+        }
+
+        break;
+    }
+
+    return numberInput;
+}
+
+/**
+ * @brief Retrieves a text from the user.
+ * @note This function keeps asking for input until a non-empty string
+ *       is provided.
+ * @param requestLine: The line to request an input with.
+ * @retval A `string` line provided by user.
+ */
+string GetStringInput(string requestLine) {
+    // Defining required input variable
+    string rawInput;
+
+    while (true) {
+        // Asking for input
+        cout << "[>] " << requestLine << ": ";
+        cin >> rawInput;
+
+        // Validating the string,
+        // if succeeded -> break the loop,
+        // if failed -> iterate again
+        if (rawInput.length() >= 1 && rawInput.at(0) != ' ') {
+            break;
+        }
+
+        cout << "[!] Ungültige Eingabe! Bitte versuchen Sie nochmal.\n |" << endl;
+    }
+
+    return rawInput;
+}
+
+/**
+ * @brief Outputs a container into CLI.
+ * @note If the vector is empty, a respective message
+ *       will be printed instead.
+ * @param v: The vector to output values of.
+ */
+void PrintContainer(vector<Book> v) {
+    if (v.size() == 0) {
+        cout << " |-<!> Es gibt keine Bücher :(" << endl;
+        return;
+    }
+
+    Book iBook;
+    for (size_t i = 0; i < v.size(); i++) {
+        iBook = v.at(i);
+
+        cout << " |-< [" << i << "]: ";
+        cout << iBook.name << " - ";
+        cout << iBook.year << " - ";
+        cout << '(' << iBook.id << ')' << endl;
+    }
+}
+
+#pragma endregion [A3.2]
