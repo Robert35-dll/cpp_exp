ed1e51d3fdf3b7f86d1f778e43f12f862b5c4050
Robert35-dll <Robert.Koch@Student.HTW-Berlin.de>
2025-11-28 14:56:39 +0100

    Renamed used functions to match the override requirement Also improved output statements

--- A5/code_a5.cpp
+++ A5/code_a5.cpp
@@ -4,17 +4,20 @@ using namespace std;
 
 // A5.1 Functions
 void TestSwapping();
-void SwapWithReference(int &, int &);
-void SwapWithPointer(int*, int*);
+void SwapInts(int &, int &);
+void SwapInts(int*, int*);
 
 int main()
 {
     // A5.1
     cout << "[*] A5.1\n |" << endl;
     TestSwapping();
+    cout << " |" << endl;
 
     return 0;
 }
+
+#pragma region [A5.1]
 
 void TestSwapping()
 {
@@ -24,30 +27,33 @@ void TestSwapping()
     cout << " |-> Initial value of a: " << a << endl;
     cout << " |-> Initial value of b: " << b << endl;
 
-    SwapWithReference(a, b);
+    SwapInts(a, b);
 
-    cout << " |-> Swapped value of a: " << a << endl;
-    cout << " |-> Swapped value of b: " << b << endl;
+    cout << " |-< Swapped value of a: " << a << endl;
+    cout << " |-< Swapped value of b: " << b << endl;
+    cout << " |" << endl;
 
     cout << " |-> Initial value of a: " << a << endl;
     cout << " |-> Initial value of b: " << b << endl;
 
-    SwapWithPointer(&a, &b);
+    SwapInts(&a, &b);
 
-    cout << " |-> Swapped value of a: " << a << endl;
-    cout << " |-> Swapped value of b: " << b << endl;
+    cout << " |-< Swapped value of a: " << a << endl;
+    cout << " |-< Swapped value of b: " << b << endl;
 }
 
-void SwapWithReference(int &a, int &b)
+void SwapInts(int &a, int &b)
 {
     int c = a;
     a = b;
     b = c;
 }
 
-void SwapWithPointer(int *a, int *b)
+void SwapInts(int *a, int *b)
 {
     int c = *a;
     *a = *b;
     *b = c;
 }
+
+#pragma endregion [A5.1]
