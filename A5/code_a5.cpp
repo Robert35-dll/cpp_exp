f8dad5338d3b1cc4d47246b9921e5c2388489887
Robert35-dll <Robert.Koch@Student.HTW-Berlin.de>
2025-11-28 14:57:12 +0100

    Added solution to A5.2

--- A5/code_a5.cpp
+++ A5/code_a5.cpp
@@ -7,11 +7,22 @@ void TestSwapping();
 void SwapInts(int &, int &);
 void SwapInts(int*, int*);
 
+// A5.2 Functions
+void TestArrayOutputs();
+void FillArray(int*, int, int, int);
+void PrintArray(int*, int*);
+void PrintArray(int*, int);
+
 int main()
 {
     // A5.1
     cout << "[*] A5.1\n |" << endl;
     TestSwapping();
+    cout << " |" << endl;
+
+    // A5.2
+    cout << "[*] A5.1\n |" << endl;
+    TestArrayOutputs();
     cout << " |" << endl;
 
     return 0;
@@ -57,3 +68,44 @@ void SwapInts(int *a, int *b)
 }
 
 #pragma endregion [A5.1]
+#pragma region [A5.2]
+
+void TestArrayOutputs()
+{
+    int arrSize = 5;
+    int arr[arrSize];
+    FillArray(arr, arrSize, 0, 10);
+    
+    cout << "[<] Array output with two pointers:" << endl;
+    PrintArray(arr, &(arr[arrSize - 1]));
+    cout << " |" << endl;
+    cout << "[<] Array output with one pointer and array size:" << endl;
+    PrintArray(arr, arrSize);
+}
+
+void FillArray(int *arrStart, int arrSize, int min = 0, int max = 1)
+{
+    srand(time(0));
+    for (int i = 0; i < arrSize; i++)
+    {
+        *(arrStart + i) = rand() % (max - min + 1) + min;
+    }
+}
+
+void PrintArray(int *arrStart, int *arrEnd)
+{
+    for (int *i = arrStart; i <= arrEnd; i++)
+    {
+        cout << "[" << i - arrStart  + 1 << "]-< " << *i << endl;
+    }
+}
+
+void PrintArray(int *arr, int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+    {
+        cout << "[" << i + 1 << "]-< " << arr[i] << endl;
+    }
+}
+
+#pragma endregion [A5.2]
